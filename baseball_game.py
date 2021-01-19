5e428a54cfd2701d542672eed5b9ff941eff24aa
gustn1104@gmail.com <hyunsoo@baghyeonsuui-MacBookPro.local>
2021-01-19 23:14:48 +0900

    Update my code

--- baseball_game.py
+++ baseball_game.py
@@ -57,6 +57,7 @@ def is_between_100_and_999(user_input_number):
     #   >>> bg.is_between_100_and_999("0")
     #   False
     # '''
+    #
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
     if int(user_input_number)>=100 and int(user_input_number) < 1000:
